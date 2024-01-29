76c22bafd94ff22633fca24f28e1fdd87b2787c4
Sam Snider <sams@Sams-Main-Laptop>
2024-01-29 15:12:22 -0600

    Save/Load finished

--- scripts/warriorsscript.js
+++ scripts/warriorsscript.js
@@ -1,12 +1,19 @@
 //General Variables
 let moon = 0;
 document.getElementById("moon").innerHTML = moon;
-let season = "Leaffall";
+const seasons = ["Newleaf","Greenleaf","Leaffall","Leafbare"];
+let season = seasons[0];
 document.getElementById("season").innerHTML = season;
+
+//Function to update general variables
+function generalvariables() {
+    document.getElementById("moon").innerHTML = moon;
+    document.getElementById("season").innerHTML = season;
+}
 
 
 //Thunder Clan Variables
-const tc_members = ["Firepaw","Tigerclaw","Bluestar"];
+let tc_members = ["Firepaw","Tigerclaw","Bluestar"];
 let tc_food = 100;
 document.getElementById("tc_food").innerHTML = tc_food;
 let tc_population = tc_members.length;
@@ -25,28 +32,43 @@ function thunderclanvariables() {
 
 //Saving Functions
 
-function save(state) {
-    localStorage.setItem("token_tc_food", JSON.stringify(tc_food));
+function save() {
+    //save general variables
+    localStorage.setItem("moon", JSON.stringify(moon));
+
+    //save ThunderClan variables
+    localStorage.setItem("tc_food", JSON.stringify(tc_food));
+    localStorage.setItem("tc_members", JSON.stringify(tc_members));
 }
 
 function load() {
-    tc_food = number(localStorage.getItem("token_tc_food"));
+    //load general variables
+    moon = localStorage.getItem("moon");
+
+    //load ThunderClan variables
+    tc_food = localStorage.getItem("tc_food");
+    tc_members = JSON.parse(localStorage.getItem("tc_members"));
+
+    //reload all displayed variables
+    generalvariables();
     thunderclanvariables();
 }
 
 function testtoken() {
-    alert(localStorage.getItem("token_tc_food"));
+    console.log(localStorage.getItem("tc_food"));
+    console.log(localStorage.getItem("tc_members"));
+    console.log(localStorage.getItem("moon"));
 }
 
 //General Functions
 function nextmoon() {
     //General Updates
     moon++
-    document.getElementById("moon").innerHTML = moon;
+    generalvariables();
 
     //Thunder Clan Updates
     tc_food = tc_food -= tc_foodcost;
-    document.getElementById("tc_food").innerHTML = tc_food;
+    thunderclanvariables();
 }
 
 //ThunderClan Functions
@@ -64,4 +86,14 @@ function tcincrementmembers() {
     let new_member = prompt("Name this new kit!")
     tc_members.push(new_member);
     thunderclanvariables();
+}
+
+//Non-game functions
+function dropdown() {
+    let x = document.getElementById("devTools");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
