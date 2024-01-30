593ec4bbc0538e898028864e3b9a88cfce9a4419
Sam Snider <sams@Sams-Main-Laptop>
2024-01-29 20:12:52 -0600

    Update warriorsscript.js and warriors.html

--- scripts/warriorsscript.js
+++ scripts/warriorsscript.js
@@ -1,99 +1,170 @@
-//General Variables
-let moon = 0;
-document.getElementById("moon").innerHTML = moon;
+//Set Gamestate
+GameState = {
+    moon: 0
+};
+
+//Global Arrays
 const seasons = ["Newleaf","Greenleaf","Leaffall","Leafbare"];
-let season = seasons[0];
-document.getElementById("season").innerHTML = season;
+
+//General Variables
+GameState.season = seasons[0];
 
 //Function to update general variables
 function generalvariables() {
-    document.getElementById("moon").innerHTML = moon;
-    document.getElementById("season").innerHTML = season;
+    document.getElementById("moon").innerHTML = GameState.moon;
+    document.getElementById("season").innerHTML = GameState.season;
 }
 
 
 //Thunder Clan Variables
-let tc_members = ["Firepaw","Tigerclaw","Bluestar"];
-let tc_food = 100;
-document.getElementById("tc_food").innerHTML = tc_food;
-let tc_population = tc_members.length;
-document.getElementById("tc_population").innerHTML = tc_population;
-let tc_foodcost = tc_population*2
-document.getElementById("tc_foodcost").innerHTML = tc_foodcost;
+
+//ThunderClan object
+ThunderClan = {
+    food: 100,
+    members:[
+    {
+        prefix: "Fire",
+        suffix: "paw",
+        title: "Apprentice",
+        age: 80
+    },{
+        prefix: "Tiger",
+        suffix: "claw",
+        title: "Warrior"
+    },{
+        prefix: "Blue",
+        suffix: "star",
+        title: "Leader"
+    }]
+}
+
+ShadowClan = {
+    food: 100,
+    members:[
+    {
+        prefix: "Broken",
+        suffix: "star",
+        title: "Leader",
+        age: 80
+    }]
+}
+
+WindClan = {
+    food: 100,
+    members:[
+    {
+        prefix: "Tall",
+        suffix: "star",
+        title: "Leader",
+        age: 80
+    }]
+}
+
+RiverClan = {
+    food: 100,
+    members:[
+    {
+        prefix: "Crooked",
+        suffix: "star",
+        title: "Leader",
+        age: 80
+    }]
+}
+
+const clans = [ThunderClan,ShadowClan,WindClan,RiverClan];
 
 //Function to update ThunderClan variables
-function thunderclanvariables() {
-    tc_population = tc_members.length;
-        document.getElementById("tc_population").innerHTML = tc_population;
-    tc_foodcost = tc_population*2
-        document.getElementById("tc_foodcost").innerHTML = tc_foodcost;
-        document.getElementById("tc_food").innerHTML = tc_food;
+function clanvariables() {
+//Update variables
+
+    for (let x in clans) {
+        clans[x].population = clans[x].members.length;
+        clans[x].foodcost = clans[x].population*2;
+    }
+
+//Update HTML variables
+    document.getElementById("tc_food").innerHTML = ThunderClan.food;
+    document.getElementById("tc_population").innerHTML = ThunderClan.population;
+    document.getElementById("tc_foodcost").innerHTML = ThunderClan.foodcost;
 }
 
 //Saving Functions
-
 function save() {
     //save general variables
-    localStorage.setItem("moon", JSON.stringify(moon));
+    localStorage.setItem("moon", JSON.stringify(GameState));
 
     //save ThunderClan variables
-    localStorage.setItem("tc_food", JSON.stringify(tc_food));
-    localStorage.setItem("tc_members", JSON.stringify(tc_members));
+    localStorage.setItem("ThunderClan", JSON.stringify(ThunderClan));
 }
 
 function load() {
-    //load general variables
-    moon = localStorage.getItem("moon");
+    //load States
+    GameState = JSON.parse(localStorage.getItem("GameState"));
+    ThunderClan = JSON.parse(localStorage.getItem("ThunderClan"));
 
-    //load ThunderClan variables
-    tc_food = localStorage.getItem("tc_food");
-    tc_members = JSON.parse(localStorage.getItem("tc_members"));
-
-    //reload all displayed variables
-    generalvariables();
-    thunderclanvariables();
+    //Update Variables
+    initialize();
 }
 
 function testtoken() {
-    console.log(localStorage.getItem("tc_food"));
-    console.log(localStorage.getItem("tc_members"));
-    console.log(localStorage.getItem("moon"));
+    console.log(localStorage.getItem("GameState"));
+    console.log(localStorage.getItem("ThunderClan"));
 }
 
 //General Functions
 function nextmoon() {
     //General Updates
-    moon++
-    generalvariables();
+    GameState.moon++
 
     //Thunder Clan Updates
-    tc_food = tc_food -= tc_foodcost;
-    thunderclanvariables();
+    for (let i in clans) {
+        incrementfood(clans[i],-clans[i].foodcost);
+        console.log(clans[i].food)
+    }
+    //Update Variables
+    initialize();
 }
 
 //ThunderClan Functions
-function tclistmembers() {
-    alert(tc_members)
+
+function listmembers(clan) {
+    let membernames = " ";
+    for (let x in clan.members) {
+        membernames += clan.members[x].prefix + clan.members[x].suffix + ", "
+    }
+    alert(membernames)
 }
 
-function tcincrementfood() {
-    tc_food++
-    document.getElementById("tc_food").innerHTML = tc_food;
-    thunderclanvariables();
+function incrementfood(clan,amount) {
+    clan.food += amount;
+    initialize();
 }
 
 function tcincrementmembers() {
-    let new_member = prompt("Name this new kit!")
-    tc_members.push(new_member);
-    thunderclanvariables();
+    let new_member = prompt("Give this kit a prefix!")
+    ThunderClan.members.push({
+        prefix: new_member,
+        suffix: "kit",
+        title: "Kitten"
+    });
+    clanvariables();
 }
 
 //Non-game functions
-function dropdown() {
-    let x = document.getElementById("devTools");
+function dropdown(id) {
+    let x = document.getElementById(id);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+}
+
+function initialize() {
+    clanvariables();
+    generalvariables();
+}
+
+function testarray() {
+    alert(clans[1].food)
 }
